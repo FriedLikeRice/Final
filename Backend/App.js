3d3f1c84961b586a974de48f2f370e3e33189f53
Nimo <shirille21@gmail.com>
2024-06-04 21:52:04 -0500

    .

--- Backend/App.js
+++ Backend/App.js
@@ -6,7 +6,7 @@ const authRoutes = require('./routes/auth');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 
 // Middleware
 app.use(express.json());
